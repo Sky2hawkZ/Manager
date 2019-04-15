727a98516e37eac9a6f8c535f115f2989ff88615
Hangfish <dan.lofgrens@gmail.com>
2019-04-15 12:48:12 +0200

    Removed Firebase code from public

--- src/app.js
+++ src/app.js
@@ -2,21 +2,12 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import firebase from 'firebase';
 import reducers from './reducers';
+import AuthenticationKey from './firebase';
 
 class App extends Component {
     componentDidMount() {
-        const config = {
-            apiKey: "AIzaSyBXrsU7tXH1OcaOcA0GstcJvtgF6bUuLtU",
-            authDomain: "manager-49355.firebaseapp.com",
-            databaseURL: "https://manager-49355.firebaseio.com",
-            projectId: "manager-49355",
-            storageBucket: "manager-49355.appspot.com",
-            messagingSenderId: "1057744163238"
-        };
 
-        firebase.initializeApp(config);
     }
 
     render() {
